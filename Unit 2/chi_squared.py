8a9c47f46ba1a0483233dcb06a775fc73bbafde1
Andreilys <andreilyskov@gmail.com>
2016-09-03 19:23:03 -0400

    chi_Square_Distribution

---
+++ Unit 2/chi_squared.py
@@ -0,0 +1,16 @@
+from scipy import stats
+import collections
+import pandas as pd
+import matplotlib.pyplot as plt
+
+loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+chi, p = stats.chisquare(freq.values())
+
+print(p)
+print(chi)
+print("The chi-squared test shows that {0} is the p and {1} is the chi".format(p, chi))
