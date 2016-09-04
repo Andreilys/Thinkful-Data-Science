7468ad226ca9fcdffe2d7f002f63f5f05eea4271
Andreilys <andreilyskov@gmail.com>
2016-09-04 16:17:58 -0400

    more work with lending club data

---
+++ Unit 2/linear_regression.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+
+loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
+
+
+cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
+
+
+loansData['Interest.Rate'] = cleanInterestRate
+
+cleanLoanLength = loansData['Loan.Length'].map(lambda x: int(x.rstrip('months')))
+
+loansData['Loan.Length'] = cleanLoanLength
+
+
+cleanFICORange = loansData['FICO.Range'].map(lambda x: x.split('-'))
+cleanFICORange.map(lambda x: [int(n) for n in x])
+loansData['FICO.Range'] = cleanFICORange
+
+ficoScore = []
+for i in range(len(loansData['FICO.Range'])):
+    ficoScore.append(int(loansData['FICO.Range'].values[i][0]))
+
+loansData['FICO.Score'] = ficoScore
+
+# plt.figure()
+# p = loansData['FICO.Score'].hist()
+# plt.show()
+
+# plt.figure()
+# a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+# plt.show()
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+# The dependent variable
+y = np.matrix(intrate).transpose()
+# The independent variables shaped as columns
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+
+x = np.column_stack([x1,x2])
+
+X = sm.add_constant(x)
+model = sm.OLS(y,X)
+f = model.fit()
+
+print(f.summary())
