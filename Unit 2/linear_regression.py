7b343391e0d014112a11c00763a2691f6bcbc895
Andreilys <andreilyskov@gmail.com>
2016-09-05 13:00:53 -0400

    regressions

--- Unit 2/linear_regression.py
+++ Unit 2/linear_regression.py
@@ -36,17 +36,19 @@ loansData['FICO.Score'] = ficoScore
 intrate = loansData['Interest.Rate']
 loanamt = loansData['Amount.Requested']
 fico = loansData['FICO.Score']
+#
+# # The dependent variable
+# y = np.matrix(intrate).transpose()
+# # The independent variables shaped as columns
+# x1 = np.matrix(fico).transpose()
+# x2 = np.matrix(loanamt).transpose()
+#
+# x = np.column_stack([x1,x2])
+#
+# X = sm.add_constant(x)
+# model = sm.OLS(y,X)
+# f = model.fit()
+#
+# print(f.summary())
 
-# The dependent variable
-y = np.matrix(intrate).transpose()
-# The independent variables shaped as columns
-x1 = np.matrix(fico).transpose()
-x2 = np.matrix(loanamt).transpose()
-
-x = np.column_stack([x1,x2])
-
-X = sm.add_constant(x)
-model = sm.OLS(y,X)
-f = model.fit()
-
-print(f.summary())
+loansData.to_csv('loansData_clean.csv', header=True, index=False)
---
+++ Unit 2/logistic_regression.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import statsmodels.api as sm
+
+loansData = pd.read_csv('loansData_clean.csv')
+
+print(loansData['Interest.Rate'])
+
+interestLessThan12 = []
+for i in range(len(loansData)):
+    if loansData['Interest.Rate'][i] >= 0.12:
+        interestLessThan12.append(1)
+    else:
+        interestLessThan12.append(0)
+
+loansData['IR_TF'] = interestLessThan12
+
+loansData['intercept'] = [1]*len(loansData)
+
+loansData['ind_vars'] = ["loan Amount", "FICO Score", "interest Rate", "interestLessThan12", "intercept"]
+
+logit = sm.Logit(loansData['IR_TF'], loansData[ind_vars])
+result = logit.fit()
+
+coeff = result.params
+print(coeff)
