134870d136ed06cdc616175a9d6589a226d75255
Andrei Lyskov <andreilyskov@gmail.com>
2016-09-17 09:52:25 -0400

    overfitting python data

--- Unit 2/test.py
+++ Unit 2/test.py
@@ -1,25 +1,3 @@
-def fizzbuzz_test(f):
-    output = []
-    for n in range(100):
-        output.append(str(f(n) + "\n"))
+import re
 
-    expected = open("fizzbuzz-output.txt, "r")
-    i = 0
-    for line in expected:
-        if line == output[i]:
-            print("Success!")
-            i += 1
-        else:
-            print("Nope. Try again.")
-
-# print(weather_data[1][2])
-
-def fizzbuzz(n):
-    ret = ""
-    if not (n%3):
-        ret += "fizz"
-    if not (n%5):
-        ret += "buzz"
-    return ret or str(n)
-
-fizzbuzz_test(fizzbuzz)
+strings = (r'these', r"are", r'''all''', r"""strings""")
